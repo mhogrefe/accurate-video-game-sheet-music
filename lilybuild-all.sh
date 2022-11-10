931919d96ad604096c8866dcfb931b6bba821a87
Mikhail Hogrefe <mhogrefe@users.noreply.github.com>
2022-11-09 22:19:39 -0500

    added SMB underworld

--- lilybuild-all.sh
+++ lilybuild-all.sh
@@ -7,4 +7,5 @@ bash lilybuild.sh nes/donkey-kong/06-round-clear/06-round-clear &&
 bash lilybuild.sh nes/donkey-kong/07-jumpman-theme-2/07-jumpman-theme-2 &&
 bash lilybuild.sh nes/donkey-kong/08-pause/08-pause &&
 bash lilybuild.sh nes/donkey-kong/09-kong-dropped/09-kong-dropped &&
-bash lilybuild.sh nes/super-mario-bros/01-overworld/01-overworld
+bash lilybuild.sh nes/super-mario-bros/01-overworld/01-overworld &&
+bash lilybuild.sh nes/super-mario-bros/02-underworld/02-underworld
